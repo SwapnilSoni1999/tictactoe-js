ce24cdb165fbfa379a6afeeebc8aa8f5178611a8
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2018-03-22 16:40:40 +0530

    add function to check if Corners are empty or not

--- js/ai.js
+++ js/ai.js
@@ -1,7 +1,50 @@
-function checkPossibility(current){
-	console.log(current);
-}
+var i=0;
 
-function placeCornur(){
-	
+function checkCornur(){
+	switch(i){
+		case 0:{
+			if(sectionSpace[0] == 0){
+				IsEmpty = true;
+				return IsEmpty;
+			}
+			else{
+				i++;
+				IsEmpty = false;
+				checkCornur();
+			}
+		}
+		case 1:{
+			if(sectionSpace[2] == 0){
+				IsEmpty = true;
+				return IsEmpty;
+			}
+			else{
+				i++;
+				IsEmpty = false;
+				checkCornur();
+			}
+		}
+		case 2:{
+			if(sectionSpace[6] == 0){
+				IsEmpty = true;
+				return IsEmpty;
+			}
+			else{
+				i++;
+				IsEmpty = false;
+				checkCornur();
+			}
+		}
+		case 3:{
+			if(sectionSpace[8] == 0){
+				IsEmpty = true;
+				return IsEmpty;
+			}
+			else{
+				i++;
+				IsEmpty = false;
+				checkCornur();
+			}
+		}
+	}	
 }
